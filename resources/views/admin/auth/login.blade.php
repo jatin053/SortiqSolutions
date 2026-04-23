9ec20ee10d83d1d82f82e327290c4e80b985b74e
Jatin Choudhary <jatinchoudhary053@gmail.com>
2026-04-23 14:00:36 +0530

    Fix admin CSS asset path

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -8,7 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fraunces:opsz,wght@9..144,600;9..144,700&family=Plus+Jakarta+Sans:wght@500;600;700;800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+    <link rel="stylesheet" href="/css/admin.css">
 </head>
 <body class="auth-page">
     <div class="auth-shell">
